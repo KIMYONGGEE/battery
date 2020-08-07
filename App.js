ce59cb538a58e087fdc3bcad5d9c34816e0f6931
SeokLii <k1nder@naver.com>
2020-08-07 17:05:26 +0900

    SeokLii's Fisrt commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to bye working on your app!</Text>
+      <Text>준수횽 바봉 용기 멍충이</Text>
       <StatusBar style="auto" />
     </View>
   );
