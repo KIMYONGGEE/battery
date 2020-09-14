681f06c69295d75e03793721100f6db10b37c8f6
KIMYONGGEE <ykkim6872@gmail.com>
2020-09-15 01:13:18 +0900

    에러 상태 추가
    에러 상태 추가

--- component/detail/sections/Description.js
+++ component/detail/sections/Description.js
@@ -11,6 +11,15 @@ function Description({ navigation, Battery, Data}) {
   var tte = Data[8] + (Data[9]*256); // 분 
   var temperature = (Data[10] + (Data[11]*256))/10 + 273.15  // 셀시어스   
   var soh = Data[12]; // %? hex? Ten?
+  var SG = Data[13];
+  var PF = Data[14];
+  var CNR = Data[15];;
+  var charging = Data[16];//0 or 1 
+
+  var LEV2ERROR ="";
+  var LEV1ERROR ="";
+  var statu="NORMAL";
+  
 
  /* const [voltage, SetVoltage] = useState();
   const [cyclecount, SetCyclecount] = useState();
@@ -34,6 +43,30 @@ function Description({ navigation, Battery, Data}) {
 
   
   });*/  
+  console.log("충전" + SG);
+
+  //Level 2 ERROR(SG) :
+  if(SG!=0){ 
+    statu = "ERROR!";
+    if(SG==1) LEV2ERROR = "LOW BAT";
+    else if(SG==2) LEV2ERROR = "O.T.D";
+    else if(SG==3) LEV2ERROR = "O.T.C";
+    else if(SG==4) LEV2ERROR = "U.T.C";
+    else if(SG==5) LEV2ERROR = "O.V";
+    else if(SG==6) LEV2ERROR = "O.C.D";
+    else if(SG==7) LEV2ERROR = "O.C.C";
+    else if(SG==8) LEV2ERROR = "O.T.I";
+    else LEV2ERROR = ""
+  }
+
+  // Level 1 ERROR(PF) : 
+  if(PF!=0){
+    statu = "ERROR!";
+    if(PF==1) LEV1ERROR = "PF STATUS VALUE";
+    else if(PF==2) LEV1ERROR = "C.F";
+    else if(PF==3) LEV1ERROR = "L.V";
+    else LEV1ERROR = ""
+  }
   return (
     <>
       <View style={styles.Data}>
@@ -46,6 +79,8 @@ function Description({ navigation, Battery, Data}) {
           <Text style={{fontSize: size*4.5, fontWeight: 'bold', color:'#F5D0A9'}}>ㆍ TEMP</Text>
           <Text style={{fontSize: size*4.5, fontWeight: 'bold', color:'#F5D0A9'}}>ㆍ SOH</Text>
           <Text style={{fontSize: size*4.5, fontWeight: 'bold', color:'#F5D0A9'}}>ㆍ STATUS</Text>
+          <Text style={{fontSize: size*3.5, fontWeight: 'bold', color:'#F5D0A9'}}>ㆍ   LEVEL1 ERROR</Text>
+          <Text style={{fontSize: size*3.5, fontWeight: 'bold', color:'#F5D0A9'}}>ㆍ   LEVEL2 ERROR</Text>
         </View>
         <View style={styles.Dataval}>
           <Text style={{fontSize: size*4.5, color:'#FBF5EF'}}>{voltage} V</Text>
@@ -54,7 +89,9 @@ function Description({ navigation, Battery, Data}) {
           <Text style={{fontSize: size*4.5, color:'#FBF5EF'}}>{tte} min</Text>
           <Text style={{fontSize: size*4.5, color:'#FBF5EF'}}>{temperature} °C</Text>
           <Text style={{fontSize: size*4.5, color:'#FBF5EF'}}>{soh} %</Text>
-          <Text style={{fontSize: size*4.5, color:'#FBF5EF'}}>{Battery[9]}</Text>
+          <Text style={{fontSize: size*4.5, color:'#FBF5EF'}}>{statu}</Text>
+          <Text style={{fontSize: size*3.5, fontWeight: 'bold', color:'red'}}>{LEV1ERROR}</Text>
+          <Text style={{fontSize: size*3.5, fontWeight: 'bold', color:'red'}}>{LEV2ERROR}</Text>
         </View>
       </View>
 
