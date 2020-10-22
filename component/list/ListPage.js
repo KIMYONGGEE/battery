b4c8fb06cd7fc3a3befee7c1556de2fcc5693830
JunSuHeo <wmfkf95@gmail.com>
2020-10-22 20:27:21 +0900

    리스트 페이지 수정

--- component/list/ListPage.js
+++ component/list/ListPage.js
@@ -8,12 +8,10 @@ import { ScrollView } from 'react-native-gesture-handler';
 //page
 import BatteryInfo from './sections/BatteryInfo';
 
-const window = Dimensions.get('window');
+// const window = Dimensions.get('window');
 
 const BleManagerModule = NativeModules.BleManager;
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
-
-
 
 export default function ListPage({navigation, route}){
 
@@ -23,20 +21,11 @@ export default function ListPage({navigation, route}){
   //SCAN해서 찾은 Beacon 저장하는 State
   const [peripherals, setPeripherals] = useState(new Map());
   var scanperipherals = new Set();
-  const [discover, setDiscover] = useState(false);
-  
-  //추가, 삭제된 Beacon들을 구분하기위해 SCAN에서 찾은 해당 id를 저장하는 State
-  const [peripheralsID, setPeripheralsID] = useState(new Array());
-  const [updatePeripheralsID, setUpdatePeripheralsID] = useState(new Array());
-
-  //기존의 peripherals에서 추가, 삭제된 값을 저장하는 State
-  const [updatePeripherals, setUpdatePeripherals] = useState(new Map());
   
   const [refreshing, setRefreshing] = useState(false);
-  const [test, setTest] = useState(false);
 
   const [appState, setAppState] = useState('');
-  const [listCnt, setListCnt] = useState(0);
+
   const list = Array.from(new Set(peripherals.values()));
 
   useEffect(() =>{
@@ -58,13 +47,11 @@ export default function ListPage({navigation, route}){
       handlerDiscover.remove();
       handlerStop.remove();
       handlerDisconnect.remove();
-      // console.log("return function");
     };
 
   }, []);
 
   useEffect(() =>{
-    // console.log("st/art hooks");
     
     if (Platform.OS === 'android' && Platform.Version >= 23) {
         PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
@@ -92,14 +79,6 @@ export default function ListPage({navigation, route}){
       // Failure code
       console.log("The user refuse to enable bluetooth");
     });
-
-    // return () => {
-    //   handlerConnect.remove();
-    //   handlerDiscover.remove();
-    //   handlerStop.remove();
-    //   handlerDisconnect.remove();
-    //   // console.log("return function");
-    // };
   }, [scanning, connecting]);
 
   const handleAppStateChange = (nextAppState)  => {
@@ -138,15 +117,10 @@ export default function ListPage({navigation, route}){
 
   const startScan = () => {
     console.log("Scanning start");
-    // async function A(){
-      BleManager.scan([], 10, true).then((results) => { //7초이상 권장사항
-        
-      });
-    // }
-    // A();
-    // setTimeout(() => {
-    //   update();
-    // }, 10000);
+
+    BleManager.scan([], 10, true).then((results) => { //7초이상 권장사항
+    });
+
     setScanning(true);
   }
 
@@ -170,53 +144,14 @@ export default function ListPage({navigation, route}){
       }
 
       if(j == scankeys.length){
-        console.log("없는거", peripheralskeys[i], typeof(peripherals));
-        setPeripherals(new Map(peripherals.delete(peripheralskeys[i])));
-        list = Array.from(new Set(peripherals.values()));
+        var tmp = peripheralskeys[i];
+        console.log("없는거", tmp, typeof(peripherals));
+        console.log(peripherals.delete(tmp))
+        setPeripherals(new Map(peripherals));
       }
     }
 
     console.log("update finish", peripherals.size, scankeys.length);
-
-
-    // // console.log("Update Start~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-    // let updateID = Array.from(new Set(peripheralsID)); //새로 들어온 peripheralsID를 저장하는 배열
-    // let existID = updatePeripheralsID;                 //기존에 있던 peripheralsID를 저장하는 배열
-    // let noneID = new Array();                          //비교해서 들어있지 않은 인덱스를 저장하는 배열
-    
-    // //추가 삭제한 부분을 처리해주는 부분
-    // //console.log("추가된 값(updtaID) : " + updateID);
-
-    // if(existID.length != 0) //기존에 들어있는 값이 있을때에만 비교를 한다. 새로들어온 값은 항상 비교한다. 
-    // {
-    //   //사라진 값이 있는지 비교
-    //   //console.log("기존 값(existID)   : " + existID);
-    //   existID.forEach(function(item, index){
-    //     var checkNone = 0;
-    //     for(var a = 0; a < updateID.length; a++){ //비교를 해서 check가 0이면 버리는 값이라서 noneID에 저장
-    //       if(item == updateID[a]){
-    //         checkNone++;
-    //       }
-    //     }
-    //     if(checkNone === 0){
-    //       //console.log(item);
-    //       noneID.push(item);
-    //     }
-    //   });
-      
-    //   //사라진값 삭제
-    //   //console.log("버릴 값            : " + noneID);
-    //   for(var a = 0; a < noneID.length; a++){
-    //     peripherals.delete(noneID[a]);
-    //     // setListCnt(listCnt - 1);
-    //     // console.log("Disount = ", listCnt);
-    //   }     
-    // }
-    
-    // //값 변경하기
-    // setUpdatePeripherals(peripherals);
-    // setUpdatePeripheralsID(updateID);
-    // setPeripheralsID(new Array()); //스캔이 끝나면 비워서 새로운 값을 받을 수 있게 한다.
     
     scanperipherals.clear();
     console.log("Update finish");
@@ -237,62 +172,29 @@ export default function ListPage({navigation, route}){
 
 
     if(checkShit <= 7){    //NEOSEMI를 찾았을때 실행하는 부분
-      console.log("find");
       scanperipherals.add(peripheral.id);
       setPeripherals(new Map(peripherals.set(peripheral.id, peripheral)));
 
-      console.log(peripherals.size, scanperipherals.size);
-
-
-      // var localperipherals = peripherals;
-      // var InputPeripheralsID = peripheralsID;
-      // var check=0;
-
-      // // console.log(peripheral.id);
-      // // console.log('Got ble peripheral', peripheral);
-
-      // if (!peripheral.name) {
-      //   peripheral.name = 'NO NAME';
-      // }
-      // //중복 방지를 위한 IF문
-      // if(Array.from(peripherals.keys()).length == 0){ //초기값은 그냥 넣는다. 
-      //   localperipherals.set(peripheral.id, peripheral);
-      //   setPeripherals(localperipherals);
-      //   console.log("First found");
-
-      // }else{ //그 이후에는 비교하며 넣는다.
-      //   Array.from(peripherals.keys()).forEach(function(item, index){
-      //     if(item == peripheral.id)check++;
-      //   });
-      //     if(check == 0){
-      //     console.log("Another found");
-      //     //SCAN하면서 peripherals에 데이터를 바로 저장한다. 
-      //     localperipherals.set(peripheral.id, peripheral);
-      //     setPeripherals(localperipherals);
-      //     // setListCnt(listCnt + 1);
-      //     // console.log("Count = ", updatePeripherals.size);
-      //   }
-      // }
-      // //SCAN한 peripheral의 id 값을 저장한다.
-      // InputPeripheralsID.push(peripheral.id);
-      // setPeripheralsID(InputPeripheralsID);
+      console.log("find", peripherals.size, scanperipherals.size);
     } 
   }
+
+
  const onRefresh =() =>{
    setRefreshing(true);
    reff();
  }
 
  const reff = () => {
-    BleManager.stopScan(); //스캔 스탑
+    // BleManager.stopScan(); //스캔 스탑
 
-    updatePeripheralsID.forEach(function(e, i){ //디스커넥트 다 하기
-      BleManager.disconnect(e).then(console.log("디스커넥트", e));
-    })
+    // updatePeripheralsID.forEach(function(e, i){ //디스커넥트 다 하기
+    //   BleManager.disconnect(e).then(console.log("디스커넥트", e));
+    // })
     
-    updatePeripherals.clear();//저장된거 비우기
-    setScanning(false);
-    setRefreshing(false);
+    // updatePeripherals.clear();//저장된거 비우기
+    // setScanning(false);
+    // setRefreshing(false);
  }
   return (
     <>
