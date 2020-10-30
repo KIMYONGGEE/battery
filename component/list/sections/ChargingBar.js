6b1b3f3dc6c0b3cf57025244f2599ec1f30fba2a
KIMYONGGEE <ykkim6872@gmail.com>
2020-10-30 19:45:18 +0900

    list page 수정
    list page 리스트 배터리 이미지 수정

--- component/list/sections/ChargingBar.js
+++ component/list/sections/ChargingBar.js
@@ -10,29 +10,29 @@ function ChargingBar({Battery, Chargestate, Batteryerr}){
     if(Chargestate==0){
       //console.log("no charging , code :", Chargestate);
       if(Batteryerr!=0){
-        if(Battery  == 100) setImagepath(require('../../../assets/main/e100.png'));
-        else if(Battery  >= 90 && Battery  < 100) setImagepath(require('../../../assets/main/e90.png'));
-        else if(Battery  >= 80 && Battery  < 90) setImagepath(require('../../../assets/main/e80.png'));
-        else if(Battery  >= 70 && Battery  < 80) setImagepath(require('../../../assets/main/e70.png'));
-        else if(Battery  >= 60 && Battery  < 70) setImagepath(require('../../../assets/main/e60.png'));
-        else if(Battery  >= 50 && Battery  < 60) setImagepath(require('../../../assets/main/e50.png'));
-        else if(Battery  >= 40 && Battery  < 50) setImagepath(require('../../../assets/main/e40.png'));
-        else if(Battery  >= 30 && Battery  < 40) setImagepath(require('../../../assets/main/e30.png'));
-        else if(Battery  >= 20 && Battery  < 30) setImagepath(require('../../../assets/main/e20.png'));
-        else if(Battery  >= 10 && Battery  < 20) setImagepath(require('../../../assets/main/e10.png'));
+        if(Battery  >= 96 && Battery  <= 100) setImagepath(require('../../../assets/main/e100.png'));
+        else if(Battery  >= 91 && Battery  < 96) setImagepath(require('../../../assets/main/e90.png'));
+        else if(Battery  >= 81 && Battery  < 91) setImagepath(require('../../../assets/main/e80.png'));
+        else if(Battery  >= 71 && Battery  < 81) setImagepath(require('../../../assets/main/e70.png'));
+        else if(Battery  >= 61 && Battery  < 71) setImagepath(require('../../../assets/main/e60.png'));
+        else if(Battery  >= 51 && Battery  < 61) setImagepath(require('../../../assets/main/e50.png'));
+        else if(Battery  >= 41 && Battery  < 51) setImagepath(require('../../../assets/main/e40.png'));
+        else if(Battery  >= 31 && Battery  < 41) setImagepath(require('../../../assets/main/e30.png'));
+        else if(Battery  >= 21 && Battery  < 31) setImagepath(require('../../../assets/main/e20.png'));
+        else if(Battery  >= 1 && Battery  < 21) setImagepath(require('../../../assets/main/e10.png'));
         else setImagepath(require('../../../assets/main/e0.png'));
       }
       else{
-        if(Battery  == 100) setImagepath(require('../../../assets/main/100.png'));
-        else if(Battery  >= 90 && Battery  < 100) setImagepath(require('../../../assets/main/90.png'));
-        else if(Battery  >= 80 && Battery  < 90) setImagepath(require('../../../assets/main/80.png'));
-        else if(Battery  >= 70 && Battery  < 80) setImagepath(require('../../../assets/main/70.png'));
-        else if(Battery  >= 60 && Battery  < 70) setImagepath(require('../../../assets/main/60.png'));
-        else if(Battery  >= 50 && Battery  < 60) setImagepath(require('../../../assets/main/50.png'));
-        else if(Battery  >= 40 && Battery  < 50) setImagepath(require('../../../assets/main/40.png'));
-        else if(Battery  >= 30 && Battery  < 40) setImagepath(require('../../../assets/main/30.png'));
-        else if(Battery  >= 20 && Battery  < 30) setImagepath(require('../../../assets/main/20.png'));
-        else if(Battery  >= 10 && Battery  < 20) setImagepath(require('../../../assets/main/10.png'));
+        if(Battery  >= 96 && Battery  <= 100) setImagepath(require('../../../assets/main/100.png'));
+        else if(Battery  >= 91 && Battery  < 96) setImagepath(require('../../../assets/main/90.png'));
+        else if(Battery  >= 81 && Battery  < 91) setImagepath(require('../../../assets/main/80.png'));
+        else if(Battery  >= 71 && Battery  < 81) setImagepath(require('../../../assets/main/70.png'));
+        else if(Battery  >= 61 && Battery  < 71) setImagepath(require('../../../assets/main/60.png'));
+        else if(Battery  >= 51 && Battery  < 61) setImagepath(require('../../../assets/main/50.png'));
+        else if(Battery  >= 41 && Battery  < 51) setImagepath(require('../../../assets/main/40.png'));
+        else if(Battery  >= 31 && Battery  < 41) setImagepath(require('../../../assets/main/30.png'));
+        else if(Battery  >= 21 && Battery  < 31) setImagepath(require('../../../assets/main/20.png'));
+        else if(Battery  >= 1 && Battery  < 21) setImagepath(require('../../../assets/main/10.png'));
         else setImagepath(require('../../../assets/main/0.png'));
       }
     }
@@ -40,29 +40,29 @@ function ChargingBar({Battery, Chargestate, Batteryerr}){
     else if(Chargestate==1){
       //console.log("charging , code :", Chargestate);
       if(Batteryerr!=0){
-        if(Battery  == 100) setImagepath(require('../../../assets/main/e100.png'));
-        else if(Battery  >= 90 && Battery  < 100) setImagepath(require('../../../assets/main/e90.png'));
-        else if(Battery  >= 80 && Battery  < 90) setImagepath(require('../../../assets/main/e80.png'));
-        else if(Battery  >= 70 && Battery  < 80) setImagepath(require('../../../assets/main/e70.png'));
-        else if(Battery  >= 60 && Battery  < 70) setImagepath(require('../../../assets/main/e60.png'));
-        else if(Battery  >= 50 && Battery  < 60) setImagepath(require('../../../assets/main/e50.png'));
-        else if(Battery  >= 40 && Battery  < 50) setImagepath(require('../../../assets/main/e40.png'));
-        else if(Battery  >= 30 && Battery  < 40) setImagepath(require('../../../assets/main/e30.png'));
-        else if(Battery  >= 20 && Battery  < 30) setImagepath(require('../../../assets/main/e20.png'));
-        else if(Battery  >= 10 && Battery  < 20) setImagepath(require('../../../assets/main/e10.png'));
+        if(Battery  >= 96 && Battery  <= 100) setImagepath(require('../../../assets/main/e100.png'));
+        else if(Battery  >= 91 && Battery  < 96) setImagepath(require('../../../assets/main/e90.png'));
+        else if(Battery  >= 81 && Battery  < 91) setImagepath(require('../../../assets/main/e80.png'));
+        else if(Battery  >= 71 && Battery  < 81) setImagepath(require('../../../assets/main/e70.png'));
+        else if(Battery  >= 61 && Battery  < 71) setImagepath(require('../../../assets/main/e60.png'));
+        else if(Battery  >= 51 && Battery  < 61) setImagepath(require('../../../assets/main/e50.png'));
+        else if(Battery  >= 41 && Battery  < 51) setImagepath(require('../../../assets/main/e40.png'));
+        else if(Battery  >= 31 && Battery  < 41) setImagepath(require('../../../assets/main/e30.png'));
+        else if(Battery  >= 21 && Battery  < 31) setImagepath(require('../../../assets/main/e20.png'));
+        else if(Battery  >= 1 && Battery  < 21) setImagepath(require('../../../assets/main/e10.png'));
         else setImagepath(require('../../../assets/main/e0.png'));
       }
       else{
-        if(Battery  == 100) setImagepath(require('../../../assets/main/c100.png'));
-        else if(Battery  >= 90 && Battery  < 100) setImagepath(require('../../../assets/main/c90.png'));
-        else if(Battery  >= 80 && Battery  < 90) setImagepath(require('../../../assets/main/c80.png'));
-        else if(Battery  >= 70 && Battery  < 80) setImagepath(require('../../../assets/main/c70.png'));
-        else if(Battery  >= 60 && Battery  < 70) setImagepath(require('../../../assets/main/c60.png'));
-        else if(Battery  >= 50 && Battery  < 60) setImagepath(require('../../../assets/main/c50.png'));
-        else if(Battery  >= 40 && Battery  < 50) setImagepath(require('../../../assets/main/c40.png'));
-        else if(Battery  >= 30 && Battery  < 40) setImagepath(require('../../../assets/main/c30.png'));
-        else if(Battery  >= 20 && Battery  < 30) setImagepath(require('../../../assets/main/c20.png'));
-        else if(Battery  >= 10 && Battery  < 20) setImagepath(require('../../../assets/main/c10.png'));
+        if(Battery  >= 96 && Battery  <= 100) setImagepath(require('../../../assets/main/c100.png'));
+        else if(Battery  >= 91 && Battery  < 96) setImagepath(require('../../../assets/main/c90.png'));
+        else if(Battery  >= 81 && Battery  < 91) setImagepath(require('../../../assets/main/c80.png'));
+        else if(Battery  >= 71 && Battery  < 81) setImagepath(require('../../../assets/main/c70.png'));
+        else if(Battery  >= 61 && Battery  < 71) setImagepath(require('../../../assets/main/c60.png'));
+        else if(Battery  >= 51 && Battery  < 61) setImagepath(require('../../../assets/main/c50.png'));
+        else if(Battery  >= 41 && Battery  < 51) setImagepath(require('../../../assets/main/c40.png'));
+        else if(Battery  >= 31 && Battery  < 41) setImagepath(require('../../../assets/main/c30.png'));
+        else if(Battery  >= 21 && Battery  < 31) setImagepath(require('../../../assets/main/c20.png'));
+        else if(Battery  >= 1 && Battery  < 21) setImagepath(require('../../../assets/main/c10.png'));
         else setImagepath(require('../../../assets/main/c0.png'));
       }
     }
